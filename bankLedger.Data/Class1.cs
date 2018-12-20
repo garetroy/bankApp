843711b091108f9a2409f56efa5c654c6a80874b
Garett Roberts <mygarett@gmail.com>
2018-12-19 17:29:13 -0800

    Basic structure setup

---
+++ bankLedger.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace bankLedger.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ bankLedger.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace bankLedger.Models
+{
+    public class Class1
+    {
+    }
+}
