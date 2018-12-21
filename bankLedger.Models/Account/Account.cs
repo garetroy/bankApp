507cf7585a18c429adf1d0ee4fb50f5e4e9c0f8d
Garett Roberts <mygarett@gmail.com>
2018-12-21 02:46:06 -0800

    Basic Account Model fininshed

--- bankLedger.Models/Account/Account.cs
+++ bankLedger.Models/Account/Account.cs
@@ -3,8 +3,18 @@ namespace bankLedger.Models
 {
     public class Account
     {
-        public Account()
+        public Account(string userName, string password, string salt,
+                        DateTime? lastLogin)
         {
+            UserName = userName;
+            Password = password;
+            Salt = salt;
+            LastLogin = lastLogin;
         }
+
+        public string UserName { get; }
+        public string Password { get; }
+        public string Salt { get; }
+        public DateTime? LastLogin { get; }
     }
 }
