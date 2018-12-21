0424e56d9b8a67d670b194dbb23bfad2203ff655
Garett Roberts <mygarett@gmail.com>
2018-12-21 02:48:27 -0800

    Created DbAccount and DbLedger Objects

--- bankLedger.Data/DbObject/DbAccount.cs
+++ bankLedger.Data/DbObject/DbAccount.cs
@@ -3,8 +3,9 @@ namespace bankLedger.Data.DbObject
 {
     internal sealed class DbAccount
     {
-        public DbAccount()
-        {
-        }
+        public string User_Name { get; set; }
+        public string Encrypted_Password { get; set; }
+        public string Salt { get; set; }
+        public DateTime? Last_Login { get; set; }
     }
 }
--- bankLedger.Data/DbObject/DbLedger.cs
+++ bankLedger.Data/DbObject/DbLedger.cs
@@ -3,8 +3,7 @@ namespace bankLedger.Data.DbObject
 {
     internal sealed class DbLedger
     {
-        public DbLedger()
-        {
-        }
+        public int Transaction_Type { get; set; }
+        public decimal Amount { get; set; }
     }
 }
