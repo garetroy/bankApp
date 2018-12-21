663c61908fabb1b01c8b814d3e4f901431257397
Garett Roberts <mygarett@gmail.com>
2018-12-21 03:49:36 -0800

    Made all projects have uniform frameworks. Added Typeconfig

---
+++ bankLedger.Web/App_Start/TypeConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using StructureMap;
+using StructureMap.Web;
+using bankLedger.Models;
+using bankLedger.Data.Services;
+
+namespace bankLedger.Web.App_Start
+{
+    public class TypeConfig
+    {
+        public static IContainer RegisterTypes()
+        {
+            return new Container(x =>
+            {
+                x.For<IBankLedgerService>().HttpContextScoped().Use(c => new BankLedgerService());
+            });
+        }
+    }
+}
