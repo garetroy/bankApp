27cd767c70705271bf2f15b2a2a2d0aa083675b3
Garett Roberts <mygarett@gmail.com>
2018-12-28 15:46:46 -0800

    Moved fakesession class to its own file

---
+++ bankLedger.Web.Tests/ControllerTests/FakeSessionState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Web;
+
+namespace bankLedger.Web.Tests.ControllerTests
+{
+
+    public class FakeSessionState : HttpSessionStateBase
+    {
+        Dictionary<string, object> items = new Dictionary<string, object>();
+        public override object this[string name]
+        {
+            get { return items.ContainsKey(name) ? items[name] : null; }
+            set { items[name] = value; }
+        }
+    }
+}
