6bd7008e8a579950b3fdb7fac8cc4dea70d83733
Garett Roberts <mygarett@gmail.com>
2019-01-15 15:22:55 -0800

    Updated packages, models, and references

--- bankLedger.Models/Account/IAccountService.cs
+++ bankLedger.Models/Account/IAccountService.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Web;
-using System.Web.SessionState;
+﻿using System.Web;
 
 namespace bankLedger.Models
 {
     public interface IAccountService
     {
         Account CreateAccount(string userName, string decryptedPassowrd);
+
         Account SignIn(string userName, string decryptedPassowrd, HttpSessionStateBase session);
+
         Account IsSignedIn(HttpSessionStateBase session);
+
         void SignOut(HttpSessionStateBase session);
     }
 }
